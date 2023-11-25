3714dd059283f5d295d65a4a2aeef189bc34a81b
Dimitar Slaev <dimslaev@gmail.com>
2023-11-25 22:27:28 +0100

    region

--- pages/api/auth/reset-password.route.ts
+++ pages/api/auth/reset-password.route.ts
@@ -4,6 +4,8 @@ import { isValid } from "@/lib/server/utils";
 import { prisma } from "@/lib/server/prisma";
 import { ResetPasswordSchema } from "@/pages/api/auth/_schemas";
 
+export const preferredRegion = "eur3";
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
